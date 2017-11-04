80f0479cc7de8d5be35ce8c3b9cae35d62f09bed
cmsegui <cmsegui@yahoo.com>
2017-11-03 21:53:12 -0400

    creating a new todo item is complete

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,13 +1,21 @@
-//make list item look completed
-$("li").click(function () {
+//make todo item look completed
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
-//delete list item
-$("span").click(function() {
-    $(this).parent().fadeOut(500, function() {
-        $(this).remove();
-    });
-    event.stopPropagation();
+//delete todo item
+$("ul").on("click", "span", function() {
+  $(this).parent().fadeOut(500, function() {
+  $(this).remove();
+});
+  event.stopPropagation();
 })
 
+//create a new todo item
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoText +"</li>")
+  }
+})
