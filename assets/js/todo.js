a66560ab1768737162b16a530f3c90476e04c285
cmsegui <cmsegui@yahoo.com>
2017-11-03 21:26:01 -0400

    used jQuery to add functionality of deleting to do items

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,13 @@
+//make list item look completed
 $("li").click(function () {
   $(this).toggleClass("completed");
 });
+
+//delete list item
+$("span").click(function() {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
